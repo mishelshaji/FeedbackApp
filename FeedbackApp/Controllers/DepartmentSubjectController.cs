61ab555ac50645a155bf157b4020ae28f1c5c11d
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-07 10:25:42 +0530

    Added DepartmentSubject API

---
+++ FeedbackApp/Controllers/DepartmentSubjectController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FeedbackApp.Models;
+
+namespace FeedbackApp.Controllers
+{
+    public class DepartmentSubjectController : ApiController
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/StudentSubject/5
+        public IEnumerable<Subject> Get(int departmentId)
+        {
+            var dept = db.Subjects.Where(m => m.DepartmentId == departmentId).ToList();
+            return dept;
+        }
+    }
+}
