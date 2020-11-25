1bd53ddb8d5fd359fa2fb7d1f4a4d49ea9534f3d
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-25 10:44:16 +0530

    Added Subject Name in feedbacks api

--- FeedbackApp/Controllers/FacultyController.cs
+++ FeedbackApp/Controllers/FacultyController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using FeedbackApp.Models;
+using FeedbackApp.Models.ViewModels;
 
 namespace FeedbackApp.Controllers
 {
@@ -13,10 +14,38 @@ namespace FeedbackApp.Controllers
         ApplicationDbContext _db = new ApplicationDbContext();
 
         // GET: api/Faculty/5
-        public ICollection<Feedback> Get(string StaffId)
+        public ICollection<FeedbackView> Get(string StaffId)
         {
-            var feedback = _db.Feedbacks.Where(m => m.TeacherId == StaffId).ToList();
-            return feedback;
+            var feedbacks = _db.Feedbacks.Where(m => m.TeacherId == StaffId).ToList();
+            var feedbackList = new List<FeedbackView>();
+            foreach (var i in feedbacks)
+            {
+                var subject = _db.Subjects.FirstOrDefault(m => m.Id == i.SubjectId);
+                if (subject == null)
+                {
+                    continue;
+                }
+                feedbackList.Add(new FeedbackView()
+                {
+                    Id = i.Id,
+                    Comment = i.Comment,
+                    StudentId = i.StudentId,
+                    PostedOn = i.PostedOn,
+                    TeacherId = i.TeacherId,
+                    SubjectName = subject.SubjectName,
+                    Option1 = i.Option1,
+                    Option2 = i.Option2,
+                    Option3 = i.Option3,
+                    Option4 = i.Option4,
+                    Option5 = i.Option5,
+                    Option6 = i.Option6,
+                    Option7 = i.Option7,
+                    Option8 = i.Option8,
+                    Option9 = i.Option9,
+                });
+            }
+
+            return feedbackList;
         }
     }
 }
---
+++ FeedbackApp/Models/ViewModels/FeedbackView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeedbackApp.Models.ViewModels
+{
+    public class FeedbackView
+    {
+        public int Id { get; set; }
+
+        public string StudentId { get; set; }
+
+        public string TeacherId { get; set; }
+
+        public int SubjectId { get; set; }
+
+        public string SubjectName { get; set; }
+
+        public DateTime PostedOn { get; set; }
+
+        public int Option1 { get; set; }
+        public int Option2 { get; set; }
+        public int Option3 { get; set; }
+        public int Option4 { get; set; }
+        public int Option5 { get; set; }
+        public int Option6 { get; set; }
+        public int Option7 { get; set; }
+        public int Option8 { get; set; }
+        public int Option9 { get; set; }
+        public string Comment { get; set; }
+    }
+}
